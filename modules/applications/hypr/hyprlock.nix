8d219aad2827f162f9b71a0aa75529e8f24be6c2
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2024-05-11 19:20:51 +0100

    Hpyrlock/hypridle home-manager updates
    - removed manual home-manager imports for hyprlock/hypridle
    - home-manager updated to include hyprlock/hypridle options
    - updated modules to reflect correct home-manager module syntax

--- modules/applications/hypr/hyprlock.nix
+++ modules/applications/hypr/hyprlock.nix
@@ -18,8 +18,10 @@
       programs = {
         hyprlock = {
           enable = true;
-          general = {
-            grace = 30;
+          settings = {
+            general = {
+              grace = 30;
+            };
           };
         };
       };
@@ -28,44 +30,46 @@
     (mkIf (config.applications.hypr.apps.hyprlock && hostname == "gibson") {
       programs = {
         hyprlock = {
-          backgrounds = [
-            {
-              monitor = "DP-1";
-              path = "${config.xdg.configHome}/Wallpapers/hex_lockscreen_middle.png";
-              color = "rgba(0, 0, 0, 1)";
-              blur_size = 0;
-              blur_passes = 0;
-              noise = 0.0;
-              contrast = 0.0;
-              brightness = 0.0;
-              vibrancy = 0.0;
-              vibrancy_darkness = 0.0;
-            }
-            {
-              monitor = "DP-2";
-              path = "${config.xdg.configHome}/Wallpapers/hex_lockscreen_left.png";
-              color = "rgba(0, 0, 0, 1)";
-              blur_size = 0;
-              blur_passes = 0;
-              noise = 0.0;
-              contrast = 0.0;
-              brightness = 0.0;
-              vibrancy = 0.0;
-              vibrancy_darkness = 0.0;
-            }
-            {
-              monitor = "HDMI-A-2";
-              path = "${config.xdg.configHome}/Wallpapers/hex_lockscreen_right.png";
-              color = "rgba(0, 0, 0, 1)";
-              blur_size = 0;
-              blur_passes = 0;
-              noise = 0.0;
-              contrast = 0.0;
-              brightness = 0.0;
-              vibrancy = 0.0;
-              vibrancy_darkness = 0.0;
-            }
-          ];
+          settings = {
+            backgrounds = [
+              {
+                monitor = "DP-1";
+                path = "${config.xdg.configHome}/Wallpapers/hex_lockscreen_middle.png";
+                color = "rgba(0, 0, 0, 1)";
+                blur_size = 0;
+                blur_passes = 0;
+                noise = 0.0;
+                contrast = 0.0;
+                brightness = 0.0;
+                vibrancy = 0.0;
+                vibrancy_darkness = 0.0;
+              }
+              {
+                monitor = "DP-2";
+                path = "${config.xdg.configHome}/Wallpapers/hex_lockscreen_left.png";
+                color = "rgba(0, 0, 0, 1)";
+                blur_size = 0;
+                blur_passes = 0;
+                noise = 0.0;
+                contrast = 0.0;
+                brightness = 0.0;
+                vibrancy = 0.0;
+                vibrancy_darkness = 0.0;
+              }
+              {
+                monitor = "HDMI-A-2";
+                path = "${config.xdg.configHome}/Wallpapers/hex_lockscreen_right.png";
+                color = "rgba(0, 0, 0, 1)";
+                blur_size = 0;
+                blur_passes = 0;
+                noise = 0.0;
+                contrast = 0.0;
+                brightness = 0.0;
+                vibrancy = 0.0;
+                vibrancy_darkness = 0.0;
+              }
+            ];
+          };
         };
       };
     })
@@ -73,39 +77,41 @@
     (mkIf (config.applications.hypr.apps.hyprlock && hostname == "gibson") {
       programs = {
         hyprlock = {
-          input-fields = [
-            {
-              monitor = "DP-1";
-              fade_on_empty = false;
-              dots_center = true;
-              size = {
-                width = 250;
-                height = 50;
-              };
-              outer_color = "rgb(255, 184, 108)";
-              inner_color = "rgb(0, 0, 0)";
-              font_color = "rgb(255, 255, 255)";
-            }
-          ];
+          settings = {
+            input-fields = [
+              {
+                monitor = "DP-1";
+                fade_on_empty = false;
+                dots_center = true;
+                size = {
+                  width = 250;
+                  height = 50;
+                };
+                outer_color = "rgb(255, 184, 108)";
+                inner_color = "rgb(0, 0, 0)";
+                font_color = "rgb(255, 255, 255)";
+              }
+            ];
 
-          labels = [
-            {
-              monitor = "DP-1";
-              text = "cmd[update:10000] echo \"$(date +'%R')\"";
-              font_family = "Hack";
-              position = {
-                x = 0;
-                y = 80;
-              };
-              color = "rgba(255, 255, 255, 1)";
-              font_size = 60;
+            labels = [
+              {
+                monitor = "DP-1";
+                text = "cmd[update:10000] echo \"$(date +'%R')\"";
+                font_family = "Hack";
+                position = {
+                  x = 0;
+                  y = 80;
+                };
+                color = "rgba(255, 255, 255, 1)";
+                font_size = 60;
 
-              shadow_passes = 1;
-              shadow_size = 6;
-              shadow_color = "rgba(255, 184, 108, 0.5)";
-              shadow_boost = 1.6;
-            }
-          ];
+                shadow_passes = 1;
+                shadow_size = 6;
+                shadow_color = "rgba(255, 184, 108, 0.5)";
+                shadow_boost = 1.6;
+              }
+            ];
+          };
         };
       };
     })
