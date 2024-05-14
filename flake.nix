4fa64adfdd2fcb4d672510c07b310cb67f532b24
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2024-05-14 21:09:35 +0100

    pkg: hyprland bump
    - Bumped to a more recent git revision of Hyprland
    - This is to fix working inconsistent with system idle
    - 0.41.0 will be used once released.

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.40.0&submodules=1;";
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     };
 
     hypridle = {
