a030cb1f005fe5645bcef5695f372eacd1017622
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2024-05-14 21:28:40 +0100

    del: migrated scripts removal

--- modules/scripts/waybar/mouse_colour.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }:
-
-pkgs.writeShellApplication {
-    name = "mouse_colour";
-    runtimeInputs = with pkgs; [ rivalcfg ];
-    text = ''
-rivalcfg d reactive -a 0000ff --top-color 0000ff --middle-color 0000ff --bottom-color 0000ff -s 800 -t 0 -T 1
-    '';
-}
