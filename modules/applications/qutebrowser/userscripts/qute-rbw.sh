4bafee88165b5fbd72857d10b203b2ecedb313e5
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2024-05-14 22:24:56 +0100

    del: removed old qutebrowser script copy
    - qute-rbw still existed outside of nix config

--- modules/applications/qutebrowser/userscripts/qute-rbw.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rbw unlocked > /dev/null 2>&1
-
-if [[ "$?" -eq 1 ]]; then
-    kitty -T "rbw password prompt" rbw unlock > /dev/null 2>&1 && hyprctl dispatch focuswindow title:qutebrowser && rofi-rbw
-else
-    rofi-rbw
-fi
