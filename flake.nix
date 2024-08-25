9326100538e6672bb8247d590b72d00c8811acce
5ysk3y <5ysk3y@pm.me>
2024-08-25 13:24:39 +0100

    pkgs: Hyprland bump (0.42.0)

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
     hyprland = {
       type = "git";
-      url = "https://github.com/hyprwm/Hyprland?ref=v0.41.1";
+      url = "https://github.com/hyprwm/Hyprland?ref=v0.42.0";
       submodules = true;
     };
 
