79a369c2abab763e42227a3b33bdf0d3747db4cf
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2024-09-12 11:10:40 +0100

    override: xdg-portal/gamescope updates
    - Removed the temp xdg-hyprland-desktop-portal-hyprland entry
    - gamescope bump to 3.15.6

--- hosts/gibson/overlays/default.nix
+++ hosts/gibson/overlays/default.nix
@@ -15,40 +15,15 @@
 
     (final: prev: {
       gamescope = prev.gamescope.overrideAttrs (finalAttrs: previousAttrs: {
-        version = "3.15.2";
-        buildInputs = previousAttrs.buildInputs ++ [ pkgs.lcms2 ];
+        version = "3.15.6";
+        nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ pkgs.git ];
         src = prev.fetchFromGitHub {
           owner = "ValveSoftware";
           repo = "gamescope";
           rev = "refs/tags/${finalAttrs.version}";
           fetchSubmodules = true;
-          hash = "sha256-g6H68dYMmpQYlwhZ6b84yY/qbAP18iNrmYOWf9rL5gc=";
+          hash = "sha256-MSW949T0UL4p3XF5yhpwY6sMCSGQ9xA3LO5syu2C8tA=";
         };
-      });
-    })
-
-    (final: prev: {
-      xdg-desktop-portal-hyprland = prev.xdg-desktop-portal-hyprland.overrideAttrs (finalAttrs: previousAttrs: {
-        patches = [
-          (pkgs.fetchpatch {
-            url = "https://github.com/hyprwm/xdg-desktop-portal-hyprland/commit/5555f467f68ce7cdf1060991c24263073b95e9da.patch";
-            hash = "sha256-yNkg7GCXDPJdsE7M6J98YylnRxQWpcM5N3olix7Oc1A=";
-          })
-
-          (pkgs.fetchpatch {
-            url = "https://github.com/hyprwm/xdg-desktop-portal-hyprland/commit/0dd9af698b9386bcf25d3ea9f5017eca721831c1.patch";
-            hash = "sha256-Y6eWASHoMXVN2rYJ1rs0jy2qP81/qbHsZU+6b7XNBBg=";
-          })
-
-          (pkgs.fetchpatch {
-            url = "https://github.com/hyprwm/xdg-desktop-portal-hyprland/commit/2425e8f541525fa7409d9f26a8ffaf92a3767251.patch";
-            hash = "sha256-6dCg/U/SIjtvo07Z3tn0Hn8Xwx72nwVz6Q2cFnObonU=";
-          })
-        ];
-
-        depsBuildBuild = [
-          pkgs.pkg-config
-        ];
       });
     })
 
