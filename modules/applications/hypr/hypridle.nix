41df018fb2be112d7488cff8233e0ce3df621263
5ysk3y <5ysk3y@pm.me>
2024-10-16 18:43:37 +0100

    conf: hypridle post-suspend cmd adjustment
    - Added an extra sleep cmd to the hypridle post-suspend
    - This is in an effort to fix DPMS failing to turn on my monitors post-suspend

--- modules/applications/hypr/hypridle.nix
+++ modules/applications/hypr/hypridle.nix
@@ -84,7 +84,7 @@ in
           settings = {
             general = {
               lock_cmd = "pidof hyprlock || hyprlock --immediate";
-              after_sleep_cmd = "sleep 1; ${undim_screen.outPath}/bin/undim_screen; hyprctl --batch 'dispatch dpms on; dispatch exec makoctl mode -s default; dispatch exec openrgb -p ${config.xdg.configHome}/OpenRGB/MainBlue.orp; dispatch exec qpwgraph -ma ${config.xdg.configHome}/qpwgraph/default.qpwgraph'";
+              after_sleep_cmd = "sleep 1; ${undim_screen.outPath}/bin/undim_screen; hyprctl --batch 'sleep 2; dispatch dpms on; dispatch exec makoctl mode -s default; dispatch exec openrgb -p ${config.xdg.configHome}/OpenRGB/MainBlue.orp; dispatch exec qpwgraph -ma ${config.xdg.configHome}/qpwgraph/default.qpwgraph'";
               ignore_dbus_inhibit = false;
               ignore_systemd_inhibit = false;
             };
