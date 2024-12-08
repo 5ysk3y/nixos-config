2cea45d7fadd720fbf617ce7853e33dc10769f0c
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2024-12-08 20:24:29 +0000

    applications: hypr* updates
    - Removed 10-bit colour depth from main monitor gibson configuration (for now)
    - Moved hypridle's dim/undim screen scripts into their own nix files
    - Updated hypridle config to use new scripts location
    - Updated hypridle config to ignore dbus inhibits
    - Added an exec line to the default Hyprland config to undim_screen on first load

--- modules/applications/hypr/hypridle.nix
+++ modules/applications/hypr/hypridle.nix
@@ -2,54 +2,9 @@
 
 let
 
-  dim_screen = pkgs.writeShellApplication {
-    name = "dim_screen";
-    runtimeInputs = with pkgs; [ ddcutil hyprland gnused gawk ];
-    text = ''
-checkBrightness() {
-    ddcutil --bus="$1" getvcp 10 | awk '{print $9}' | sed 's/,//g'
-}
-dimScreen() {
-    for s in 8 9 10; do
-    BRIGHTNESS=$(checkBrightness "$s")
-      while [[ $BRIGHTNESS != "10" ]]
-      do
-          ddcutil --bus="$s" setvcp 10 10
-          BRIGHTNESS=$(checkBrightness "$s")
-      done
-    wait
-    done
-}
-
-dimScreen
-hyprctl dispatch -- exec makoctl mode -s idle
-
-    '';
-  };
-
-  undim_screen = pkgs.writeShellApplication {
-    name = "undim_screen";
-    runtimeInputs = with pkgs; [ ddcutil hyprland gnused gawk ];
-    text = ''
-checkBrightness() {
-    ddcutil --bus="$1" getvcp 10 | awk '{print $9}' | sed 's/,//g'
-}
-
-undimScreen() {
-    for s in 8 9 10; do
-    BRIGHTNESS=$(checkBrightness "$s")
-      while [[ $BRIGHTNESS != "80" ]]
-      do
-          ddcutil --bus="$s" setvcp 10 80
-          BRIGHTNESS=$(checkBrightness "$s")
-      done
-    wait
-    done
-}
-
-undimScreen
-hyprctl dispatch exec -- makoctl mode -s default
-    '';
+  scripts = rec {
+    dim_screen = (import ./scripts/dim_screen.nix {inherit pkgs;});
+    undim_screen = (import ./scripts/undim_screen.nix {inherit pkgs;});
   };
 
 in
@@ -73,15 +28,16 @@ in
           enable = true;
           settings = {
             general = {
+              ignore_dbus_inhibit = true;
               lock_cmd = "pidof hyprlock || hyprlock --immediate";
-              after_sleep_cmd = "hyprctl --batch 'sleep 1; dispatch dpms on; dispatch exec makoctl mode -s default; dispatch exec openrgb -p ${config.xdg.configHome}/OpenRGB/MainBlue.orp; dispatch exec qpwgraph -ma ${config.xdg.configHome}/qpwgraph/default.qpwgraph'; ${undim_screen.outPath}/bin/undim_screen;";
+              after_sleep_cmd = "hyprctl --batch 'sleep 1; dispatch dpms on; dispatch exec makoctl mode -s default; dispatch exec openrgb -p ${config.xdg.configHome}/OpenRGB/MainBlue.orp; dispatch exec qpwgraph -ma ${config.xdg.configHome}/qpwgraph/default.qpwgraph'; ${scripts.undim_screen.outPath}/bin/undim_screen;";
             };
 
             listener = [
               {
                 timeout = 300;
-                on-timeout = "${dim_screen.outPath}/bin/dim_screen";
-                on-resume = "${undim_screen.outPath}/bin/undim_screen";
+                on-timeout = "${scripts.dim_screen.outPath}/bin/dim_screen";
+                on-resume = "${scripts.undim_screen.outPath}/bin/undim_screen";
               }
               {
                 timeout = 360;
