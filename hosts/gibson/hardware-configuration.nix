5ysk3y/nixos-config

commit 748820555969862461ee483a0d9ef7904fd5f79c

system: removed independent bootloader file
- Merged independent bootloader file back into hardware-configuration.nix
- Cleaned up some remnant duplicate configuration

hosts/gibson/hardware-configuration.nix
@@ -9,19 +9,36 @@
     ];
 
   boot = {
+    kernelPackages = pkgs.linuxPackages_xanmod;
+    kernelModules = [ "kvm-amd" "amdgpu" "i2c-dev" "ddcci_backlight" "i2c-piix4" "it87" "k10temp" ];
+    kernelParams = [
+      "video=DP-1:2560x1440@144"
+      "video=DP-2:1920x1080@144"
+      "video=HDMI-A-2:1920x1080@60"
+      "acpi_enforce_resources=lax"
+    ];
+    extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
+    loader = {
+      systemd-boot = {
+        enable = true;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+      };
+    };
     initrd = {
       luks = {
         devices = {
           "luks-ff1a20da-b20b-4dbb-9d0d-1471a4bb141f" = {
             device = "/dev/disk/by-uuid/ff1a20da-b20b-4dbb-9d0d-1471a4bb141f";
           };
+          "luks-13479742-2586-406b-9016-048d6937f0b7" = {
+            device = "/dev/disk/by-uuid/13479741-2586-406b-9016-048d6937f0b7";
+          };
         };
       };
-      kernelModules = [ ];
       availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
     };
-    kernelModules = [ "kvm-amd" ];
-    extraModulePackages = [ ];
     resumeDevice = "/dev/disk/by-uuid/698a5b6d-64a8-49b5-956b-e84bfb809bb6";
   };
 

hosts/gibson/boot/bootloader.nix
@@ -1,20 +1,0 @@
-# Do not modify this file!  It was generated by ‘nixos-generate-config’
-# and may be overwritten by future invocations.  Please make changes
-# to /etc/nixos/configuration.nix instead.
-{ config, lib, pkgs, ... }:
-
-{
-  boot.initrd.kernelModules = [ "amdgpu" ];
-  boot.kernelParams = [
-  "video=DP-1:2560x1440@144"
-  "video=DP-2:1920x1080@144"
-  "video=HDMI-A-2:1920x1080@60"
-  "acpi_enforce_resources=lax"
-  "acpi_backlight=native"
-];
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  boot.kernelModules = [ "kvm-amd" "i2c-dev" "i2c-piix4" "it87" "k10temp" ];
-  boot.loader.systemd-boot.enable = true;  
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.luks.devices."luks-13479742-2586-406b-9016-048d6937f0b7".device = "/dev/disk/by-uuid/13479741-2586-406b-9016-048d6937f0b7";
-}

commit 5dc69751aaf5c194e75bf43121290ee79aef1736

conf: gibson hardware updates
- Moved to stable xanmod kernel (again.. I'm kernel hopping like a madman at the moment)
- Re-added `efi.canTouchEfiVariables`
  > There is an open issue for this atm in the systemd github repo (35497)
  > This is actively impacting efivars for hibernation
- Added `amdgpu` to the initrd kernelModules list; this has fixed most issues with hibernate for me
  > The above EFI vars issue still remains on nixos-rebuild's post-hibernate
  > This was an unnoticed regression introduced with commit https://github.com/5ysk3y/nixos-config/commit/748820555969862461ee483a0d9ef7904fd5f79c
- Removed the specific `resume` declaration in `boot.kernelParams`
- Updated swap device UUID following a refresh of that partition (whilst troubleshooting hibernation woes..)

hosts/gibson/hardware-configuration.nix
@@ -9,19 +9,21 @@
     ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_zen;
-    kernelModules = [ "kvm-amd" "amdgpu" "amd-zen" "i2c-dev" "i2c-piix4" "k10temp" ];
+    kernelPackages = pkgs.linuxPackages_xanmod_stable;
+    kernelModules = [ "kvm-amd" "amdgpu" "i2c-dev" "i2c-piix4" "k10temp" ];
     kernelParams = [
       "video=DP-1:2560x1440@144"
       "video=DP-2:1920x1080@144"
       "video=HDMI-A-2:1920x1080@60"
       "acpi_enforce_resources=lax"
-      "resume=/dev/disk/by-uuid/698a5b6d-64a8-49b5-956b-e84bfb809bb6"
     ];
     extraModulePackages = with config.boot.kernelPackages; [ ];
     loader = {
       systemd-boot = {
         enable = true;
+      };
+      efi = {
+        canTouchEfiVariables = true;
       };
     };
     initrd = {
@@ -38,6 +40,7 @@
       systemd = {
         enable = true;
       };
+      kernelModules = [ "amdgpu" ];
       availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
     };
   };
@@ -58,7 +61,7 @@
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/698a5b6d-64a8-49b5-956b-e84bfb809bb6"; }
+    [ { device = "/dev/disk/by-uuid/22e7db71-e91f-4a00-ab2e-43b928409160"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking