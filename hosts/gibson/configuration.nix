b60ceabe23f08fcad3b8705b78a46e760ad0d9fd
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2025-01-26 14:11:37 +0000

    conf: host update (gibson)
    - Added in wine-tkg via commit aaf367c
    - Added in nix-gaming caches to improve rebuild times
    - Added in bottles application (to play around with)
    - Added in OBS gstreamer plugin
    - Added in configuration to allow signed git commits

--- hosts/gibson/configuration.nix
+++ hosts/gibson/configuration.nix
@@ -84,6 +84,7 @@
       ddcutil
       expect
       i2c-tools
+      inputs.nix-gaming.packages.${pkgs.system}.wine-tkg
       libmodule
       lm_sensors
       nix-prefetch-github
@@ -343,10 +344,12 @@ KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
      substituters = [
        "https://hyprland.cachix.org"
        "https://cache.nixos.org"
+       "https://nix-gaming.cachix.org"
      ];
      trusted-public-keys = [
        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
      ];
    };
    gc = {
