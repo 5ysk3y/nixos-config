712ee81e7925a876c74a0e202df35bccc0a2812e
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2025-03-28 23:59:24 +0000

    overlays: temporary hyprland version bump
    - Bumped to 0.48.1 until it hits nixpkgs

--- hosts/gibson/overlays/default.nix
+++ hosts/gibson/overlays/default.nix
@@ -24,6 +24,17 @@
           gamemode
         ];
       };
+
+      hyprland = prev.hyprland.overrideAttrs (finalAttrs: previousAttrs: {
+        version = "0.48.1";
+        src = prev.fetchFromGitHub {
+          owner = "hyprwm";
+          repo = "hyprland";
+          fetchSubmodules = true;
+          tag = "v${finalAttrs.version}";
+          hash = "sha256-skuJFly6LSFfyAVy2ByNolkEwIijsTu2TxzQ9ugWarI=";
+        };
+      });
     })
   ];
 }
