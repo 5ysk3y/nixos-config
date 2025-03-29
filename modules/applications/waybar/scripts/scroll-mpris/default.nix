72e1e9016cce61fa88f67dc948fd1ea9fd3dfa74
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2025-03-29 17:27:39 +0000

    modules: waybar media player overhaul
    - Migrated away from my own bash scripted implementation with zscroll
    - Now using a rust binary solution: https://github.com/BEST8OY/ScrollMPRIS
    - This allows dynamic icon changing based on the player and is less buggy
    - Nix pins to a specific commit and implements a custom patch that:
      > Adds specific service name/icon changes
      > Shows text rather than an empty string if no player is active
      > Makes "Artist - Title" the default output
      > Replaces &'s with +'s (to avoid killing waybar)
      > Updates the default scroll wrapper/divider to "|"
      > Resets the scroll state to start upon changing track

--- modules/applications/waybar/scripts/scroll-mpris/default.nix
+++ modules/applications/waybar/scripts/scroll-mpris/default.nix
@@ -1,22 +1,26 @@
 { pkgs, ... }:
 
+# Not my repo - Check it out here: https://github.com/BEST8OY/ScrollMPRIS
+
 with pkgs;
 
 rustPlatform.buildRustPackage {
   pname = "scroll-mpris";
-  version = "0.1.0"; # update to the correct version or tag if available
+  version = "0.1.0"; 
   src = fetchFromGitHub {
     owner = "BEST8OY";
     repo = "ScrollMPRIS";
-    rev = "master"; # you can replace this with a specific commit or tag
-    sha256 = "sha256-Ea3ChIbETTJ8x9hz/lRlbseDD3yW5Rm+eV3SV2w/HPo="; # run nix-build to determine the correct hash
+    rev = "7549ba2b879aa4f3c0bfcbcc525cab23ec635453"; 
+    sha256 = "sha256-RGE/x/1sTYL3zV/t0vHog7t4X6pP1g3jJPIZqRRxjJM="; 
   };
   useFetchCargoVendor = true;
-  cargoHash = ""; # similarly, update this after first build
+  cargoHash = "sha256-GIrwUJxe/ktHnZ2NW9car1t0WHHL1Wxlc7448lmzk/c="; 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ dbus ];
   patches = [
-    ./cargo.lock.patch
-    ./update-icons.patch
+    ./customisations.patch
   ];
+  postUnpack = ''
+    cp ${./Cargo.lock} $sourceRoot/Cargo.lock
+  '';
 }
