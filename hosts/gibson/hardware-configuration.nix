5f5590e49b714de2142f55834166ec95bc97e7df
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2025-05-05 14:12:11 +0100

    system: gibson updates
    - Added amdvlk as an extraPackage to the graphics options
    - Switched to updated format for zsh init commands
    - Removed Cider-2 override for now (it wasnt working)

--- hosts/gibson/hardware-configuration.nix
+++ hosts/gibson/hardware-configuration.nix
@@ -2,7 +2,6 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, system, vars, ... }:
-
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
