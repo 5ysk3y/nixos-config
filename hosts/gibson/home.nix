08171ea396c362376549dbcb48690248e2d29a41
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2025-05-12 09:46:22 +0100

    conf: gibson updates
    - Added lutris/SDDTheme input to installed pkgs
    - Defined Vulkan ICD env var as "RADV" by default
    - Explicitly enabled dbus
    - Added vulkan-related pkgs to hardware.graphics.extraPackages
    - Re-wrote Mako config due to breaking changes with that and NixOS
    - Fixed cider-2 overlay to include a valid sha256 hash

--- hosts/gibson/home.nix
+++ hosts/gibson/home.nix
@@ -252,35 +252,39 @@ bindkey -M viins '\e.' insert-last-word
 
     mako = {
       enable = true;
-      output = "DP-1";
-      backgroundColor = "#282A36";
-      textColor = "#FFFFFF";
-      padding = "10";
-      font = "Tamzen 12";
-      layer = "overlay";
-      anchor = "top-right";
-      margin = "11";
-      defaultTimeout = 20000;
-      borderSize = 1;
-      borderRadius = 5;
-      width = 400;
-      height = 170;
-      maxIconSize = 32;
-      extraConfig =
-"[urgency=low]
-border-color=#BD93F9
-
-[urgency=normal]
-border-color=#BD93F9
-on-notify=exec ${pkgs.vlc}/bin/cvlc --play-and-exit ${config.xdg.configHome}/mako/notification.wav
-
-[urgency=high]
-border-color=#FF5555
-on-notify=exec ${pkgs.vlc}/bin/cvlc --play-and-exit ${config.xdg.configHome}/mako/notification.wav
-
-[mode=idle]
-default-timeout=0
-ignore-timeout=1";
+      criteria = {
+        "urgency=low" = {
+          border-color = "#BD93F9";
+        };
+        "urgency=normal" = {
+          border-color = "#BD93F9";
+          on-notify = "exec ${pkgs.vlc}/bin/cvlc --play-and-exit ${config.xdg.configHome}/mako/notification.wav";
+        };
+        "urgency=high" = {
+          border-color = "#FF5555";
+          on-notify = "exec ${pkgs.vlc}/bin/cvlc --play-and-exit ${config.xdg.configHome}/mako/notification.wav";
+        };
+        "mode=idle" = {
+          default-timeout = 0;
+          ignore-timeout = 1;
+        };
+      };
+      settings = {
+        output = "DP-1";
+        backgroundColor = "#282A36";
+        textColor = "#FFFFFF";
+        padding = "10";
+        font = "Tamzen 12";
+        layer = "overlay";
+        anchor = "top-right";
+        margin = "11";
+        defaultTimeout = 20000;
+        borderSize = 1;
+        borderRadius = 5;
+        width = 400;
+        height = 170;
+        maxIconSize = 32;
+      };
     };
 
    gpg-agent = {
--- hosts/gibson/overlays/default.nix
+++ hosts/gibson/overlays/default.nix
@@ -17,16 +17,15 @@
           keyutils
         ];
       };
-
-      heroic = prev.heroic.override {
-        extraPkgs = pkgs: with pkgs; [
-          gamescope
-          gamemode
-        ];
-      };
     })
 
     (self: super: {
+      cider-2 = super.cider-2.overrideAttrs (old: {
+        src = pkgs.fetchurl {
+          url = "file://${vars.syncthingPath}/Files/nix/Cider/cider-linux-x64.AppImage";
+          sha256 = "0qjhsssccxiq92zs04zhi53bkaf2qwfq7ryic1w9sha59ffyxqbf";
+        };
+      });
     })
   ];
 }
