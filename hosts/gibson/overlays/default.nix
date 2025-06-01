44adbe177be03f3264b8ee9566e49f3428e1674d
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2025-06-01 11:12:10 +0100

    overrides: another attempt to streamline cider-2

--- hosts/gibson/overlays/default.nix
+++ hosts/gibson/overlays/default.nix
@@ -17,5 +17,15 @@
         ];
       };
     })
+
+    (self: super: {
+      cider-2 = super.cider-2.overrideAttrs (_: {
+        src = pkgs.requireFile {
+          name = "cider-linux-x64.AppImage";
+          url = "https://cidercollective.itch.io/cider";
+          sha256 = "5d506132048d240613469c79186ae8b5e78ec7400f233b8709b7fe908353d9e5";
+        };
+      });
+    })
   ];
 }
