37c370ee092f77ab03ff7564e4342da5b276720e
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2025-10-10 12:41:17 +0100

    module: dim-screen update
    - Removed useFetchCargoVendor option to avoid build failures

--- modules/applications/dim-screen/default.nix
+++ modules/applications/dim-screen/default.nix
@@ -11,7 +11,6 @@ rustPlatform.buildRustPackage {
     rev = "c516aaa0482e9aec2cce1bdb42c8186c87b37379";
     sha256 = "sha256-n1CV4Gge7ugjaMIv3MHbh6Yg6ofvmjYi5046CnI8BPE=";
   };
-  useFetchCargoVendor = true;
   cargoHash = "sha256-iQT5RwL7me1vusvSyBxqgRK+Dpk+FDn7F6w+VtWCHZc=";
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ libxkbcommon ];
