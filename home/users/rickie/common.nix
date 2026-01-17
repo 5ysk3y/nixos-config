3d10ef44132be5899f04d16e7c69a756677b3b2f
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2026-01-17 19:11:10 +0000

    refactor(home): split common/host-specific HM conf
    - All config from hosts/<HOSTNAME>/home.nix has either:
    
      1) Been migrated into home/users/rickie/common.nix where it is to be used across multiple systems
      2) Been migrated into home/users/rickie/<HOSTNAME>.nix if it is host-specific config
    
    - Unused input references cleaned up where possible.

---
+++ home/users/rickie/common.nix
@@ -0,0 +1,121 @@
+{
+  config,
+  lib,
+  pkgs,
+  vars,
+  ...
+}:
+
+let
+  ghKey = pkgs.fetchurl {
+    url = "https://github.com/5ysk3y.gpg";
+    sha256 = "1w6vml01gf81mnck4gmwi91ynkhwdsw8z84lxjlz8bvbwrj6cwrx";
+  };
+in
+
+{
+  home = {
+    username = "${vars.username}";
+
+    sessionVariables = {
+      GIT_AUTO_FETCH_INTERVAL = 1200;
+    };
+
+    packages = with pkgs; [
+      nixfmt
+    ];
+
+    stateVersion = "23.11";
+  };
+
+  programs = with pkgs; {
+    home-manager = {
+      enable = true;
+    };
+
+    zsh = {
+      enable = true;
+      autosuggestion = {
+        enable = true;
+      };
+      sessionVariables = {
+        GNUMAKEFLAGS = "-j12";
+        LESSHISTFILE = "-";
+      };
+      oh-my-zsh = {
+        enable = true;
+        theme = "gentoo";
+        plugins = [
+          "sudo"
+          "git"
+          "vi-mode"
+          "git-auto-fetch"
+        ];
+      };
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [ "--cmd cd" ];
+    };
+
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "5ysk3y";
+          email = "62815243+5ysk3y@users.noreply.github.com";
+        };
+        alias = {
+          newpr = "!f() { git fetch origin -p && git checkout -B \"$1\" origin/main; }; f";
+          st = "!git status";
+        };
+        push = {
+          default = "current";
+          autoSetupRemote = "true";
+        };
+        branch = {
+          autoSetupMerge = true;
+        };
+        commit.gpgsign = true;
+        user.signingkey = "7D73BA8CF10F7F67";
+      };
+      includes = [
+        {
+          condition = "gitdir:~/nixos-config/**";
+          contents = {
+            core = {
+              hooksPath = ".githooks";
+            };
+          };
+        }
+      ];
+    };
+
+    gpg = {
+      enable = true;
+      publicKeys = [
+        {
+          source = ghKey;
+          trust = 5;
+        }
+      ];
+    };
+
+    htop = {
+      enable = true;
+      package = htop-vim;
+    };
+  };
+
+  services = {
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      enableZshIntegration = true;
+      defaultCacheTtl = 600;
+      maxCacheTtl = 7200;
+    };
+  };
+}
