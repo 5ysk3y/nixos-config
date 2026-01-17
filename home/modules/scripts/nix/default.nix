8d0fbdd2183d165f280f6aa14ff222b2e71f61d7
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2026-01-17 16:44:40 +0000

    refactor(tree); Move modules to home/modules
    - hosts/gibson is the only system using module imports currently and has been updated accordingly

---
+++ home/modules/scripts/nix/default.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  vars,
+}:
+{
+  nix-build-system = pkgs.writeShellApplication {
+    name = "nix-build-system";
+    runtimeInputs = with pkgs; [
+      nvd
+      gawk
+      gnused
+    ];
+    text = ''
+      CONFIG="${vars.nixos-config}"
+      HOME="/home/${vars.username}"
+      OUTFILE="$HOME/changes_$(date +%d-%m-%y).out"
+      cd $CONFIG
+      echo "Beginning build. This may take some time."
+      echo ""
+      systemd-inhibit --no-pager --no-legend --mode=block --who='${vars.username}' --why='NixOS System Building' nixos-rebuild --flake $CONFIG build --option eval-cache false --show-trace
+
+      echo ""
+      echo "Build complete. Providing result:"
+      echo ""
+      nvd diff /run/current-system $CONFIG/result > "$OUTFILE"
+
+      #Cleanup
+      awk -i inplace '{$0=gensub(/\s*\S+/,"",2)}1' "$OUTFILE"
+      sed -i -e '1,2d' -e 's/Version/Changed\/Updated:/g' -e 's/Added/\nAdded:/g' -e 's/Removed/\nRemoved:/g' -e '$d' "$OUTFILE"
+      rm $CONFIG/result
+
+      cat "$OUTFILE"
+
+      echo "Result has been stored in $HOME/$OUTFILE. Finished"
+    '';
+  };
+}
