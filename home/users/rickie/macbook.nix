8451171bbbbcae637f494eaed9ef526ba3d2b12e
Rickie <62815243+5ysk3y@users.noreply.github.com>
2026-01-17 19:34:36 +0000

    refactor(home): split common/host-specific HM conf (#18)
    - All config from hosts/<HOSTNAME>/home.nix has either:
    
    1) Been migrated into home/users/rickie/common.nix where it is to be
    used across multiple systems
    2) Been migrated into home/users/rickie/<HOSTNAME>.nix if it is
    host-specific config
    
    - Unused input references cleaned up where possible.

---
+++ home/users/rickie/macbook.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  pkgs,
+  vars,
+  ...
+}:
+{
+  home = {
+    homeDirectory = "/Users/${vars.username}";
+
+    sessionVariables = {
+    };
+
+    packages = with pkgs; [
+      gnupg
+      mpv
+      nixfmt
+      yt-dlp
+    ];
+  };
+
+  ## START PROGAMS
+
+  programs = {
+    zsh = {
+      shellAliases = {
+        ll = "ls -lah";
+        nixos-rebuild = "sudo darwin-rebuild switch --flake .#macbook";
+      };
+    };
+  };
+
+  ## END PROGAMS
+  ## START SERVICES
+
+  services = {
+    gpg-agent = {
+      pinentry = {
+        package = pkgs.pinentry_mac;
+      };
+    };
+  };
+
+  ## END SERVICES ##
+}
