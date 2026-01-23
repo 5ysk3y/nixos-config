1a0305070e01b59461d01f380988315e025243bb
Rickie <62815243+5ysk3y@users.noreply.github.com>
2026-01-23 15:41:54 +0000

    modules(config): mpv/jellyfin-mpv-shim updates (#26)
    - Added in updated MPV config for jellyfin-mpv-shim to use
    - Replicated newer mpv config (non-jellyfin) to specific hosts
    - Added in a new wallpaper-specific MPV profile
    - Updated mpvpaper exec cmd in hyprland config to use new profile
    - This follows a bunch of local testing to prevent freezes during video playback on Wayland/Nvidia

--- home/users/rickie/gibson.nix
+++ home/users/rickie/gibson.nix
@@ -137,25 +137,69 @@
 
     mpv = {
       enable = true;
+
       bindings = {
         q = "run \"/bin/sh\" \"-c\" \"$(which hyprctl) --batch 'dispatch killactive; dispatch workspace m-1'\"";
       };
+
       config = {
         geometry = "25%+10+10/1";
         osc = "yes";
         ontop = "yes";
-        vo = "dmabuf-wayland";
-        hwdec = "vaapi";
+
+        vo = "gpu-next";
         gpu-context = "wayland";
+        gpu-api = "opengl";
+
+        hwdec = "nvdec";
+        hwdec-codecs = "all"; # fixed key name
+
         user-agent = "Mozilla/5.0";
         cache = "yes";
         save-position-on-quit = "yes";
         ytdl-format = "bestvideo+bestaudio";
         stream-buffer-size = "5MiB";
         demuxer-max-bytes = "1G";
+
         ao = "pipewire";
-        volume = 70;
+        volume = 90;
+
+        vd-lavc-dr = "no";
       };
+
+      profiles = {
+        wallpaper = {
+          vo = "gpu";
+          gpu-context = "wayland";
+          gpu-api = "opengl";
+          hwdec = "nvdec";
+          hwdec-codecs = "all";
+
+          osc = "no";
+          input-default-bindings = "no";
+          input-vo-keyboard = "no";
+
+          deband = "no";
+          interpolation = "no";
+          deinterlace = "no";
+
+          correct-downscaling = "no";
+          scale = "bilinear";
+          cscale = "bilinear";
+          dscale = "bilinear";
+          tscale = "oversample";
+
+          save-position-on-quit = "no";
+          watch-later-options = "no";
+
+          cache = "no";
+          framedrop = "vo";
+
+          loop-file = "inf";
+          vf = "fps=30";
+        };
+      };
+
       scripts = [
         mpvScripts.mpris
       ];
