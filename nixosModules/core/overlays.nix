306b8f359c1254b82f8f5c4aad46c656c3a10d60
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2026-03-08 14:57:36 +0000

    overlay(gibson): move less from common -> gibson
    - I added this as a common overlay for all machines initially
    - This has now been moved to host-specific config
    - Rebuilds on darwin are enormous if its included
    - Darwin itself actually doesnt exhibit the bug im fixing

--- nixosModules/core/overlays.nix
+++ nixosModules/core/overlays.nix
@@ -3,15 +3,6 @@ _:
 {
   nixpkgs.overlays = [
     (final: prev: {
-      # Temporary workaround for less 691 xterm-kitty pager regression.
-      # Remove once nixpkgs includes less >= 692 everywhere we build.
-      less = prev.less.overrideAttrs (_: rec {
-        version = "692";
-        src = prev.fetchurl {
-          url = "https://www.greenwoodsoftware.com/less/less-${version}.tar.gz";
-          hash = "sha256-YTAPYDeY7PHXeGVweJ8P8/WhrPB1pvufdWg30WbjfRQ=";
-        };
-      });
     })
   ];
 }
