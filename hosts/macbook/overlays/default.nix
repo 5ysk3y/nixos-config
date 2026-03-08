fea99f3333b6bd143df5cee68575d1cef2dc229e
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2026-03-08 20:58:02 +0000

    host(macbook): remove yt-dlp overlay
    - Upstream package has been fixed so this is no longer required
    
    !! 400th commit ftw !!

--- hosts/macbook/overlays/default.nix
+++ hosts/macbook/overlays/default.nix
@@ -9,16 +9,6 @@
 {
   nixpkgs.overlays = [
     (_final: prev: {
-      # Can be rm'd when this merges into nixos-unstable: https://github.com/NixOS/nixpkgs/pull/493943
-      yt-dlp = prev.yt-dlp.overridePythonAttrs (override: {
-        dependencies = __filter (
-          dep:
-          !(__elem dep.pname [
-            "cffi"
-            "secretstorage"
-          ])
-        ) override.dependencies;
-      });
     })
   ];
 }
