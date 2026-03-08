6ba875f829d7fc284b960e7d7f200a272f852dea
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2026-03-08 12:35:22 +0000

    host(gibson): various different fixes
    - Made some changes to hyprland window rules (this needs further cleanup)
    - Disabled wayland for SDDM; to work with display outputs nicely on boot
    - Fix a badly formatted DOCKER_HOST env var
    - Shfited xserver definitions into the existing services code block
    - Added an override to fix an issue with virt-secret-init-encrpytion.service

--- hosts/gibson/overlays/default.nix
+++ hosts/gibson/overlays/default.nix
@@ -34,6 +34,29 @@
       mpv = prev.mpv.override {
         inherit (final) mpv-unwrapped;
       };
+
+      ## Can be rm'd once https://github.com/NixOS/nixpkgs/pull/496839 is merged upstream
+      libvirt = prev.libvirt.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.makeWrapper ];
+
+        buildInputs = (old.buildInputs or [ ]) ++ [ prev.systemd ];
+
+        postPatch = (old.postPatch or "") + ''
+          substituteInPlace src/secret/virt-secret-init-encryption.service.in \
+            --replace-fail /usr/bin/sh ${
+              prev.lib.getExe (
+                prev.writeShellApplication {
+                  name = "virt-secret-init-encryption-sh";
+                  runtimeInputs = [
+                    prev.coreutils
+                    prev.systemd
+                  ];
+                  text = ''exec ${prev.runtimeShell} "$@"'';
+                }
+              )
+            }
+        '';
+      });
     })
   ];
 }
