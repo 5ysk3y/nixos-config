49b40d391734db7e1d1956fcbc876aeab146dec9
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2026-03-25 00:01:33 +0000

    host(gibson): add zswap + rm libvirt overlay
    - Adds zswap config into gibsons system kernel params
    - Removes the libvirt overlay now upstream has been fixed
    - Adds a cheeky adjustement to the window rule for social apps

--- hosts/gibson/overlays/default.nix
+++ hosts/gibson/overlays/default.nix
@@ -35,29 +35,6 @@
         inherit (final) mpv-unwrapped;
       };
 
-      ## Can be rm'd once https://github.com/NixOS/nixpkgs/pull/496839 is merged upstream
-      libvirt = prev.libvirt.overrideAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.makeWrapper ];
-
-        buildInputs = (old.buildInputs or [ ]) ++ [ prev.systemd ];
-
-        postPatch = (old.postPatch or "") + ''
-          substituteInPlace src/secret/virt-secret-init-encryption.service.in \
-            --replace-fail /usr/bin/sh ${
-              prev.lib.getExe (
-                prev.writeShellApplication {
-                  name = "virt-secret-init-encryption-sh";
-                  runtimeInputs = [
-                    prev.coreutils
-                    prev.systemd
-                  ];
-                  text = ''exec ${prev.runtimeShell} "$@"'';
-                }
-              )
-            }
-        '';
-      });
-
       # Temporary workaround for less 691 xterm-kitty pager regression.
       # Remove once nixpkgs includes less >= 692 everywhere we build.
       # https://github.com/NixOS/nixpkgs/pull/490763
