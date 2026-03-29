1501bb832ee80bcbfec3d0191fa280730db16ae6
5ysk3y <62815243+5ysk3y@users.noreply.github.com>
2026-03-29 12:09:19 +0100

    host(gibson): fix upstream warnings + rm overlay
    - Removes the less 692 overlay as thats now been merged upstream
    - Fixes git/gtk warnings caused due to null defaults being setup upstream

--- hosts/gibson/overlays/default.nix
+++ hosts/gibson/overlays/default.nix
@@ -34,17 +34,6 @@
       mpv = prev.mpv.override {
         inherit (final) mpv-unwrapped;
       };
-
-      # Temporary workaround for less 691 xterm-kitty pager regression.
-      # Remove once nixpkgs includes less >= 692 everywhere we build.
-      # https://github.com/NixOS/nixpkgs/pull/490763
-      less = prev.less.overrideAttrs (_: rec {
-        version = "692";
-        src = prev.fetchurl {
-          url = "https://www.greenwoodsoftware.com/less/less-${version}.tar.gz";
-          hash = "sha256-YTAPYDeY7PHXeGVweJ8P8/WhrPB1pvufdWg30WbjfRQ=";
-        };
-      });
     })
   ];
 }
