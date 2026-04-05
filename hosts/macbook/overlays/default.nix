e85f178c9f06e774b0e4796147a336ffb4557cbe
Rickie <62815243+5ysk3y@users.noreply.github.com>
2026-04-05 16:01:26 +0100

    hosts(macbook): remove mailutils override (#111)
    - This revokes the override added in ab98b45 (#96)
    - nixpkgs has the fixed merged upstream

--- hosts/macbook/overlays/default.nix
+++ hosts/macbook/overlays/default.nix
@@ -9,13 +9,6 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      # Can be rm'd once upstream merge lands in nixos-unstable (https://nixpk.gs/pr-tracker.html?pr=503376)
-      mailutils = prev.mailutils.overrideAttrs (old: {
-        nativeCheckInputs = builtins.filter (pkg: pkg != prev.nss_wrapper) (old.nativeCheckInputs or [ ]);
-
-        preCheck = "";
-        doCheck = false;
-      });
     })
   ];
 }
