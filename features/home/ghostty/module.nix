e85fbccfc8742350881dad8e1f6d2a9417f222d1
Rickie <62815243+5ysk3y@users.noreply.github.com>
2026-04-14 18:19:00 +0100

    feature(home): add multi-platform ghostty module (#121)
    - Adds an additional ghostty shell client usable as a home-manager feature module
    - This will gate the installed package depending on system type
    - Macbook has now got this included over an imperative install of ghostty
    - Preferred settings have been retained as defaults (theme etc)

---
+++ features/home/ghostty/module.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  programs = {
+    ghostty = {
+      enable = true;
+      package = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
+      enableZshIntegration = true;
+      installVimSyntax = true;
+      settings = {
+        theme = "dracula";
+        font-size = 14;
+        macos-option-as-alt = "left";
+      };
+    };
+  };
+}
--- profiles/home/darwin.nix
+++ profiles/home/darwin.nix
@@ -3,6 +3,7 @@
   imports = [
     inputs.mac-app-util.homeManagerModules.default
     inputs.self.modules.homeManager.doomemacs
+    inputs.self.modules.homeManager.ghostty
     inputs.self.modules.homeManager.qutebrowser
     inputs.self.modules.homeManager.sops-nix
     inputs.self.modules.homeManager.symlinks
