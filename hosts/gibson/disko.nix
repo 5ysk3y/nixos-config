993fdb2a3bdf83e71b32c0aecb3d6940a7ce1af4
Rickie <62815243+5ysk3y@users.noreply.github.com>
2026-06-07 14:10:27 +0100

    fix(disko): set gibson /boot umask to 0077 (#183)
    The umask for vfat works inverted so umask=0077 would give 0700.
    Fixes warnings about global read/write on /boot for new nixos installs.
    
    - Sets umask mountOptions for /boot from 0700 -> 0077

--- hosts/gibson/disko.nix
+++ hosts/gibson/disko.nix
@@ -24,7 +24,7 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = [ "umask=0700" ];
+              mountOptions = [ "umask=0077" ];
             };
           };
 
